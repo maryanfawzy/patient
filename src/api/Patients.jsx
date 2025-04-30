c631a6c49bdba1686779c1ac8593a22cdf4bac28
maryan fawzy <maryanfawzy7@gmail.com>
2025-04-29 20:25:01 -0500

    update

--- src/api/Patients.jsx
+++ src/api/Patients.jsx
@@ -1,6 +1,15 @@
 export const fetchJessicaData = async () => {
   try {
-    const res = await fetch('https://api-coalition.tech/v1/patients');
+    const username = 'coalition';
+    const password = 'skills-test';
+    const encoded = btoa(`${username}:${password}`);
+
+    const res = await fetch('https://fedskillstest.coalitiontechnologies.workers.dev', {
+      headers: {
+        Authorization: `Basic ${encoded}`,
+      },
+    });
+
     const data = await res.json();
     const jessica = data.find((p) => p.name === 'Jessica Taylor');
     return jessica;
